46f556c376cc9f2d1eba2b0ba46b73cd2147a582
Tahsina2226 <tahsinatanvin274@gmail.com>
2024-11-03 21:44:42 +0600

    book card sesh

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,31 +1,40 @@
 import React from 'react'
 
 const Book = ({ book }) => {
-    const {image, bookName, author,tags}=book;
-    return (
-        <div className="bg-base-100 shadow-xl p-6 rounded-2xl w-96 card">
-        <figure className='bg-sky-100 py-8'>
-          <img
-            src={image}
-            className='h-[166px]'
-            alt={bookName} />
-        </figure>
-        <div className="card-body">
-       <div className='flex justify-center gap-10'>
-       {tags.map(tag => <button className="bg-green-200 h-7 text-extrabold text-green-500 btn btn-xs">{tag}</button>)}
-       </div>
-          <h2 className="card-title">
+  const { image, bookName, author, tags, category,rating} = book
+  return (
+    <div className='bg-base-100 shadow-xl p-6 rounded-2xl w-96 card'>
+      <figure className='bg-sky-100 py-8'>
+        <img src={image} className='h-[166px]' alt={bookName} />
+      </figure>
+      <div className='card-body'>
+        <div className='flex justify-center gap-10'>
+          {tags.map(tag => (
+            <button className='bg-green-200 h-7 text-extrabold text-green-500 btn btn-xs'>
+              {tag}
+            </button>
+          ))}
+        </div>
+        <h2 className='card-title'>
           {bookName}
-            <div className="badge badge-secondary">NEW</div>
-          </h2>
-          <p>By:{author}</p>
-          <div className="justify-end card-actions">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+         
+        </h2>
+        <p>By:{author}</p>
+        <div className='border-t-2 border-dashed'></div>
+
+        <div className='justify-between p-5 card-actions'>
+          <div className='badge badge-outline'>{category}</div>
+          <div className='rating'>
+            <input
+              type='radio'
+              name='rating-2'
+              className='bg-green-300 mask mask-star-2'
+            />{rating}
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Book
